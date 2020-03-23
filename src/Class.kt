73dd953d6d2e06efdd6a9e264284eede4ed7208a
Yodhaa <saurabhsingh4589@gmail.com>
2020-03-23 09:04:19 +0530

    Class and Nullability Call

---
+++ src/Class.kt
@@ -0,0 +1,24 @@
+
+package Kotlin.Class
+
+data class Person(var name:String, var age:Int){
+    init{
+        println("my name is $name and my age is $age")
+    }
+}
+
+fun main (args:Array<String>){
+
+    var h=Person("saurabh",25)
+    h.name="sahil"
+    h.age=35
+    println("my name is ${h.name} ad age is : ${h.age}")
+
+    var h1=Person("saurabh",25)
+    println(h==h1)
+
+    var h2=h.copy()
+    println(h2)
+    println(h2==h)
+
+}
---
+++ src/Nullability.kt
@@ -0,0 +1,29 @@
+package Kotlin.Nullability
+
+import java.util.*
+
+
+fun main(args:Array<String>){
+
+     nullability()
+    bitrthday()
+}
+
+fun bitrthday() {
+
+    println("Enter Year of Birth: ")
+    val yearx = readLine()!!.toInt()
+    val yeard = Calendar.getInstance().get(Calendar.YEAR)
+
+    val age = yeard - yearx
+    println("age: $age")
+
+}
+
+fun nullability() {
+
+    var y:String ? =null
+     val len:Int? = y?.length ?: -1
+    println(len)
+
+}
