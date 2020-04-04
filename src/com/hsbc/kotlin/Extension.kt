52089e73725c4e68ae7f92b38463f6750e0c871a
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-04 14:46:21 +0530

    Recursive calls and Extension Kotlin Features

---
+++ src/com/hsbc/kotlin/Extension.kt
@@ -0,0 +1,22 @@
+package com.hsbc.kotlin
+
+fun main(args:Array<String>){
+
+    var student=Student()
+    println("Pass Status: "+student.hasPassed(57))
+    println("Scholarship Status: "+student.isScholar(99))
+
+}
+
+fun Student.isScholar(marks: Int):Boolean{
+    return marks>95
+}
+
+class Student {
+
+     fun hasPassed(marks:Int):Boolean {
+
+         return marks >40
+     }
+
+}
