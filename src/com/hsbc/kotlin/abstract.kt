2036c4722e3263a931f01a2503982629b7364e43
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-04 20:11:19 +0530

    Abstract class, Method and Abstract Properties

---
+++ src/com/hsbc/kotlin/abstract.kt
@@ -0,0 +1,17 @@
+package com.hsbc.kotlin
+
+abstract class Person1{     // you cannot create instance of abstract class
+      abstract var name:String
+     abstract fun eat()             // abstract properties are open by default
+    open fun getHeight(){
+                              // A open function ready to be overridden
+    }
+    fun gotSchool(){}           // A normal function : public and final by default
+}
+
+class Indian1 :Person1(){
+    override var name:String="Saurabh"
+    override fun eat(){
+
+    }
+}
