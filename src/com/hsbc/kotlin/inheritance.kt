98fd12d82000ae6a20ead12b67510f00a4b5a9ba
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-04 16:46:19 +0530

    Inheritance with Primary Constructor call

--- src/com/hsbc/kotlin/inheritance.kt
+++ src/com/hsbc/kotlin/inheritance.kt
@@ -2,47 +2,25 @@ package com.hsbc.kotlin
 
 fun main(args:Array<String>){
 
-    var dog=Dog()
-    dog.breed="labra"
-    dog.color="black"
-    dog.bark()
-    dog.eat()
+    var dog=Dog("black","pug")
     print(dog.color)
 
-    var animal=Animal()
-    animal.color="White"
-    animal.eat()
 
   }
 
-open class Animal {
-    open var color:String =" "
-
-   open fun eat(){
-        println("Animal EAT")
+open class Animal constructor(color: String) {
+    // super
+    init {
+        println(" From Animal init: $color")
     }
 }
 
-class Dog :Animal(){
-    var breed: String =""
-    override  var color:String ="Black"
-    fun bark(){
-        println("Dog Bark")
-    }
-   override fun eat(){
-       super<Animal>.eat()
-      println(" Dog Eat")
-    }
+class Dog(var color:String , var breed:String) :Animal(color){
+
+    // derived
+     init {
+         println(" From dog init: $color and $breed")
+     }
 }
 
-class cat:Animal(){
-    var age:Int =-1
-    fun meow(){
-        println("CAt Meow")
-    }
-    override fun eat(){
-        super.eat()
-        println(" CAT Eat")
-    }
 
-}
