c3e3a3a3e5111e51f4227efa0cdcc31ca4a51276
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-05 12:48:31 +0530

    Data Classes

---
+++ src/com/hsbc/kotlin/data.kt
@@ -0,0 +1,26 @@
+package com.hsbc.kotlin
+
+fun main(args:Array<String>){
+
+    var u1=User("Sam",10)
+    var u2=User("Sam",10)
+
+    println(u1.toString())
+
+    var u3=u1.copy()
+
+    var u4=u1.copy(name = "Saurabh")     // named Parameter follows here based on name given not the order
+
+    println(u4)
+
+    if(u1==u2)
+        println("Equalls")
+    else
+        println("Not Equalls")
+}
+
+data class User(var name:String , var id:Int){
+
+    var x:Int =10   // properties must be initialised or be abstract 
+
+}
