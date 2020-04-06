5ce72f559f089020066860535c7550afa5f29870
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-06 17:58:47 +0530

    ArrayList Impl in Kotlin

--- src/com/hsbc/advanceKotlin/collections.kt
+++ src/com/hsbc/advanceKotlin/collections.kt
@@ -5,13 +5,24 @@ fun main(args:Array<String>){
     // Array Tutorials
 
     // Mutable and Fixed size
-    var myArray = Array<Int>(5){0}  // size of 5 with the each element set to '0'
+   // var myArray = Array<Int>(5){0}  // size of 5 with the each element set to '0'
 
-    for(element in myArray){
+   // var list= listOf<String>("A","B","C")  // Sequential order with Immutable,Fixed Size, Read only
+
+    var list= mutableListOf<String>("A","B","C")
+     list.add("Z")
+
+    list.remove("A")
+
+    list[0]="A"
+
+    // list[2]="V"  // Cann't do this
+
+    for(element in list){
         println(element)
     }
 
-    for (index in 0..myArray.size-1){
-        println(myArray[index])
+    for (index in 0..list.size-1){
+        println(list[index])
     }
 }
