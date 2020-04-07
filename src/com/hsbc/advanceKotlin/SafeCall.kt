81c584ab52ab9eeb1e440f53c575c0710eb8d1f9
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-08 00:55:17 +0530

    Null Safe Operators.

---
+++ src/com/hsbc/advanceKotlin/SafeCall.kt
@@ -0,0 +1,15 @@
+package com.hsbc.advanceKotlin
+
+fun main(args:Array<String>){
+
+    val name:String? = null      // assign null value to var type
+
+    println(name?.length)
+
+    println(name?.let {"($name.length)"})
+
+    println(name?.length ?: -1 )
+
+    println(name!!.length)       // throws the NullPointerException if not Found any value 
+
+}
