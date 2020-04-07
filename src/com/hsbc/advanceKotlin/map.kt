e35653d68ea99dfa685a32a3b5bc249b80e03529
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-08 00:03:55 +0530

    MAP and HASH MAP |  Mutable and Immutable

--- src/com/hsbc/advanceKotlin/map.kt
+++ src/com/hsbc/advanceKotlin/map.kt
@@ -1,2 +1,21 @@
 package com.hsbc.advanceKotlin
 
+fun main(args:Array<String>){
+
+    // var mymap= mapOf<Int,String>(1 to "A",2 to "B") // Immutable ,Fixed
+
+    var mymap = HashMap<Int,String>()
+
+      // HashMap<Int,String>() or  mutableMapOf<Int,String>() or hashMapOf<Int,String>  all are same
+     // mutableMapOf() : - >   returns LinkedHashMap 
+
+    mymap.put(1,"A")
+    mymap.put(23,"B")
+
+    mymap.replace(1,"Z")
+
+    for(key in mymap.keys){
+        println(mymap[key])   //  or write  ${mymap.get(key)}
+    }
+
+}
